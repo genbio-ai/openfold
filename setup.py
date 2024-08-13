cf217d8e157171a00c9c862cdeed2844b727539c
JiayouZhang <Jiayou.Zhang@mbzuai.ac.ae>
2024-08-13 08:37:49 -0400

    remove all compile related things in setup.py

--- setup.py
+++ setup.py
@@ -12,63 +12,63 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
+# import os
 from setuptools import setup, Extension, find_packages
-import subprocess
+# import subprocess
 
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CUDA_HOME
+# from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CUDA_HOME
 
-from scripts.utils import get_nvidia_cc
+# from scripts.utils import get_nvidia_cc
 
 
-version_dependent_macros = [
-    '-DVERSION_GE_1_1',
-    '-DVERSION_GE_1_3',
-    '-DVERSION_GE_1_5',
-]
+# version_dependent_macros = [
+#     '-DVERSION_GE_1_1',
+#     '-DVERSION_GE_1_3',
+#     '-DVERSION_GE_1_5',
+# ]
 
-extra_cuda_flags = [
-    '-std=c++14',
-    '-maxrregcount=50',
-    '-U__CUDA_NO_HALF_OPERATORS__',
-    '-U__CUDA_NO_HALF_CONVERSIONS__',
-    '--expt-relaxed-constexpr',
-    '--expt-extended-lambda'
-]
+# extra_cuda_flags = [
+#     '-std=c++14',
+#     '-maxrregcount=50',
+#     '-U__CUDA_NO_HALF_OPERATORS__',
+#     '-U__CUDA_NO_HALF_CONVERSIONS__',
+#     '--expt-relaxed-constexpr',
+#     '--expt-extended-lambda'
+# ]
 
-def get_cuda_bare_metal_version(cuda_dir):
-    raw_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "-V"], universal_newlines=True)
-    output = raw_output.split()
-    release_idx = output.index("release") + 1
-    release = output[release_idx].split(".")
-    bare_metal_major = release[0]
-    bare_metal_minor = release[1][0]
+# def get_cuda_bare_metal_version(cuda_dir):
+#     raw_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "-V"], universal_newlines=True)
+#     output = raw_output.split()
+#     release_idx = output.index("release") + 1
+#     release = output[release_idx].split(".")
+#     bare_metal_major = release[0]
+#     bare_metal_minor = release[1][0]
 
-    return raw_output, bare_metal_major, bare_metal_minor
+#     return raw_output, bare_metal_major, bare_metal_minor
 
-compute_capabilities = set([
-    (3, 7), # K80, e.g.
-    (5, 2), # Titan X
-    (6, 1), # GeForce 1000-series
-])
+# compute_capabilities = set([
+#     (3, 7), # K80, e.g.
+#     (5, 2), # Titan X
+#     (6, 1), # GeForce 1000-series
+# ])
 
-compute_capabilities.add((7, 0))
-_, bare_metal_major, _ = get_cuda_bare_metal_version(CUDA_HOME)
-if int(bare_metal_major) >= 11:
-    compute_capabilities.add((8, 0))
+# compute_capabilities.add((7, 0))
+# _, bare_metal_major, _ = get_cuda_bare_metal_version(CUDA_HOME)
+# if int(bare_metal_major) >= 11:
+#     compute_capabilities.add((8, 0))
 
-compute_capability, _ = get_nvidia_cc()
-if compute_capability is not None:
-    compute_capabilities = set([compute_capability])
+# compute_capability, _ = get_nvidia_cc()
+# if compute_capability is not None:
+#     compute_capabilities = set([compute_capability])
 
-cc_flag = []
-for major, minor in list(compute_capabilities):
-    cc_flag.extend([
-        '-gencode',
-        f'arch=compute_{major}{minor},code=sm_{major}{minor}',
-    ])
+# cc_flag = []
+# for major, minor in list(compute_capabilities):
+#     cc_flag.extend([
+#         '-gencode',
+#         f'arch=compute_{major}{minor},code=sm_{major}{minor}',
+#     ])
 
-extra_cuda_flags += cc_flag
+# extra_cuda_flags += cc_flag
 
 
 setup(
@@ -85,9 +85,6 @@ setup(
         "openfold": ['utils/kernel/csrc/*'],
         "": ["resources/stereo_chemical_props.txt"]
     },
-    install_requires=[
-        "torch",
-    ],
     # ext_modules=[CUDAExtension(
     #     name="attn_core_inplace_cuda",
     #     sources=[
